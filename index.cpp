41709505a9931de4e614ac1257e6f511af4be769
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-06 15:37:28 +0530

    Variable declaration

--- index.cpp
+++ index.cpp
@@ -5,6 +5,9 @@ using namespace std;
 
 int main(void)
 {
-
+    const int N = 3;
+    int user_turn, computer_turn;
+    int user_score = 0, computer_score = 0;
+    int max_score;
     return 0;
 }
