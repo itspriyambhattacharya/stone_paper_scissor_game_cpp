d33d495add0fc1a32c1ba1218ae1f7a27d1458ef
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2024-10-06 15:43:26 +0530

    Adding logic of the game

--- index.cpp
+++ index.cpp
@@ -12,5 +12,16 @@ int main(void)
     cout << "\nEnter max score:\t";
     cin >> max_score;
     srand(time(0)); // seed to generate random number for computer's turn
+    while (user_score != max_score || computer_score != max_score)
+    {
+        cout << "\nEnter:\n1 for Stone\n2 for Paper\n3 for Scissor\n";
+        cout << "\nEnter your turn:\t";
+        cin >> user_turn;
+        computer_turn = (rand() % N) + 1;
+        if ((user_turn == 1 && computer_turn == 1) || (user_turn == 2 && computer_turn == 2) || (user_turn == 3 && computer_turn == 3))
+        {
+            cout << "\nDraw, both entered same.\n";
+        }
+    }
     return 0;
 }
